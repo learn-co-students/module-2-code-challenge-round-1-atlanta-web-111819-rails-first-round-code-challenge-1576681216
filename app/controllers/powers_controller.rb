e2c55a325ba328ea768cf8bbc2798fae8ea038f9
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-18 11:05:11 -0500

    crushed it

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,19 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    select_power
+  end
+  
+
+private
+  def select_power
+    @power = Power.find(params[:id])
+  end
+
+  def post_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
