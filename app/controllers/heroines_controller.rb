b917f6234518772c7f7054bb9326a2f2afd864b1
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-12-18 10:31:05 -0500

    cbasic challenge complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
+  before_action :get_heroine, only: [:show]
+  
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def get_heroine
+    @heroine = Heroine.find(params.require(:id))
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:super_name, :name, :power_id)
   end
 end
